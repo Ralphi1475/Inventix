35521127d6a7b5413178a4e4ba7b4c884c87a1dc
Ralphi1475 <gathelier.biz@gmail.com>
2025-11-15 10:52:30 +0100

    suppression

--- src/app/gestion/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { DataProvider } from '@/context/DataContext';
-
-export default function GestionLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DataProvider>{children}</DataProvider>;
-}
